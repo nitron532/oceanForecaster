5f8831fe16b37144429cd672ff0214013dad5db5
nitron532 <89716148+nitron532@users.noreply.github.com>
2024-09-13 11:21:59 -0700

    delimiters, os module,
    added forloop to add delimiters to lines of data from live noaa file, imported os module to delete buoy txt file once operations are done

--- buoyMain/main.py
+++ buoyMain/main.py
@@ -1,4 +1,5 @@
 import wget
+import os
 from datetime import date
 todayDate = str(date.today())
 def readToday(fileName: str):
@@ -11,12 +12,18 @@ def readToday(fileName: str):
                 continue
             if(count == 26):
                 break
-            details.append(line)
+            stringLine = ""
+            for character in line: #
+                if character == " ":
+                    stringLine +="&"
+                else:
+                    stringLine += character
+            details.append(stringLine)
     return details
 
 #if(line.substring(8,10)!= todaysday): stop loop, return list  | has to ignore first two lines, problem is live data file uses different time zone than PST
 
-#NTBC1 and 46053 are the closest buoys to ventura point, ntbc1 is closer to shore
+#NTBC1 and 46053 are the closest buoys to ventura point, ntbc1 is closer to shore 
 
 def searchHour(dayList):
     print()
@@ -33,8 +40,7 @@ def searchHour(dayList):
     print(strQuery)
     
     for i in range(len(dayList)):
-        hour = dayList[i]
-        #print(hour)
+        hour = dayList[i] #instead of hardcoding index of hour, replace spaces with special characters, first space = year, ... MM = missing
         if(hour[11:13] == strQuery):
             windSpeed = str(float(hour[22:25]) * 3.281)
            # print(windSpeed)
@@ -43,7 +49,7 @@ def searchHour(dayList):
             waveHeight = str(float(hour[33:36]) * 3.281)
             #print(waveHeight)
             imperial = hour[0:21] + " Wind Speed: " + windSpeed + "ft/s " + "Gust Speed: " +  gustSpeed + "ft/s " + "Wave Height: " + waveHeight + "ft " + hour[36:]
-            #print(imperial)
+            print(imperial +"test")
             return imperial
 
 #for an approximate result, multiply the length value by 3.281
@@ -53,19 +59,15 @@ def searchHour(dayList):
 #2023 03 06 04 50 250  4.0  5.0   1.7    13   7.5 286 1019.2  11.9  12.5    MM   MM   MM    MM
 #0123456789
 
-#searchHour founction needs to be able to handle when spaces are different, when measurements say MM, might have to go off like the nth instance of 
-#when an index isnt a space to find where the different measurements are and keep count, like windspeed is the 5th listed measurement, 5th time 
-#loop finds an index that isnt a space i record those numbers
-
 
 buoyNumber = str(input("Input buoy number: "))
 url = 'https://www.ndbc.noaa.gov/data/realtime2/' + buoyNumber + '.txt'
 wGetFile = wget.download(url)
 buoyDataFile = readToday(wGetFile)
-#for i in range(len(buoyDataFile)):
-#    print(buoyDataFile[i])
+toDelete = str(wGetFile)
 measurements = searchHour(buoyDataFile)
-print(measurements)
+print(measurements) #showing "none"
+os.remove(toDelete)
 #input(what hour search for / what should i do now : search for hour option)
 
 
